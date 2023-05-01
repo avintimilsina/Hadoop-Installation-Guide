2c329fc6198f2b94fba91e622341eade9df94449
dip <48840584+dipta52@users.noreply.github.com>
2023-05-01 14:57:05 +0530

    Add files via upload
    Initilization

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Hadoop-Installation-Guide
-A guide to install hadoop in windows operating system
+# Hadoop Instalation Guide
+
+This is an installation guide to install hadoop in Ubuntu
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>HADOOP</span>,
+  project: {
+    link: '',
+  },
+  chat: {
+    link: '',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/core/docs/pages',
+  footer: {
+    text: 'Hadoop instalation guide',
+    
+  },
+}
+
+export default config
